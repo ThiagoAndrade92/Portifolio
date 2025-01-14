04343ea69be9d0f9b44e4b2e5efb2a33319000d4
ThiagoAndrade92 <tihaguinho92@gmail.com>
2025-01-14 17:40:54 -0300

    nav-mobile feita

--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@ const menu = document.querySelector('.menu');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('active');
+    document.querySelector('.nav-mobile').classList.toggle('active')
 })
