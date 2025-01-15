f818f51fa91eddf0a2d3530ee6000900167fcdd8
ThiagoAndrade92 <tihaguinho92@gmail.com>
2025-01-14 21:47:47 -0300

    click nav-mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector('.menu');
+const navMobile = document.querySelector('.nav-mobile');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('active');
-    document.querySelector('.nav-mobile').classList.toggle('active')
-})
+   !menu.classList.contains('active') ? navMobile.classList.remove('active') : navMobile.classList.add('active');
+});
+
+navMobile.addEventListener('click', () => {
+     navMobile.classList.remove('active');
+     menu.classList.remove('active')
+});
