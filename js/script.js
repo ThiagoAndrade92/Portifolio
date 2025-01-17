1878558b346e58bf245ddd47ee81345c292c5ba2
ThiagoAndrade92 <tihaguinho92@gmail.com>
2025-01-17 13:13:45 -0300

    corrigindo as midias

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 const menu = document.querySelector('.menu');
 const navMobile = document.querySelector('.nav-mobile');
+const navItens = document.querySelectorAll('.nav-mobile > .nav-itens > li > a');
 
 menu.addEventListener('click', () => {
-    menu.classList.toggle('active');
-   !menu.classList.contains('active') ? navMobile.classList.remove('active') : navMobile.classList.add('active');
+     menu.classList.toggle('active');
+     !menu.classList.contains('active') ? navMobile.classList.remove('active') : navMobile.classList.add('active');
 });
 
-navMobile.addEventListener('click', () => {
-     navMobile.classList.remove('active');
-     menu.classList.remove('active')
+navItens.forEach((e) => {
+     e.addEventListener('click', () => {
+          navMobile.classList.remove('active');
+          menu.classList.remove('active')
+     });
 });
